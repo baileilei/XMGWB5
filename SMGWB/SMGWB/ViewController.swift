a2aacff6003e0c98d4156d0cc8e61cdccde637ca
geduo <864029261@qq.com>
2016-05-21 09:55:01 +0800

    部署项目

---
+++ SMGWB/SMGWB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SMGWB
+//
+//  Created by baishangshang on 16/5/21.
+//  Copyright © 2016年 geduo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
